a36d10f0d3740f849ce13c4652c2f83b9071d1ae
AntoDono <anthony20151128@gmail.com>
2024-01-30 20:23:12 -0500

    test

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ const bool useDistance = false;
 const float delayBetweenMovesMS = 25; // delay between turn or move forward/backwards
 const float targetTime = 20;
 const int maxn = 7; // grid size
-const pii start = pii(4, 0); // y, x
+const pii start = pii(6, 6); // y, x
 const float moveDistance = 0.5; // in meters
 
 /*DEBUG*/
@@ -42,17 +42,17 @@ W = towards x = 0
 */
 
 //   s  l     l     l
-    {4, 0, 0, 0, 4, 1, 4},
-    {1, 3, 0, 3, 1, 3, 0}, // <- l
-    {2, 0, 0, 0, 0, 1, 0},
-    {0, 3, 0, 3, 0, 3, 0}, // <- l
-    {0, 0, 0, 1, 0, 0, 0},
-    {1, 3, 0, 3, 1, 3, 0}, // <- l
-    {4, 0, 0, 0, 4, 1, 4}
+    {0, 0, 0, 0, 0, 0, 2},
+    {0, 3, 0, 3, 1, 3, 1}, // <- l
+    {0, 0, 4, 1, 0, 0, 0},
+    {0, 3, 1, 3, 0, 3, 0}, // <- l
+    {0, 0, 0, 0, 0, 1, 4},
+    {1, 3, 1, 3, 0, 3, 1}, // <- l
+    {0, 0, 0, 0, 0, 0, 0}
 
 };
 
-int Gatezones = 6;
+int Gatezones = 2;
 
 bool graphSetupError = false;
 bool vis[maxn][maxn];
